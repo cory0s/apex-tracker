716767b89a9e3b52f59cb188f86dbaa2da1395a9
Cory Henderson <chenderson19899@gmail.com>
2019-12-31 17:04:34 -0800

    try deploy

--- apex-tracker/src/components/player/player.js
+++ apex-tracker/src/components/player/player.js
@@ -1,33 +1,41 @@
 import React from 'react';
 import axios from 'axios';
 
+
 class Player extends React.Component{
 
   constructor(props){
     super(props);
     this.state = {
-      player: [],
+      characters: {},
     }
   }
 
   componentDidMount(){
     const options = {
-      headers: {
-      'TRN-Api-Key': '213aa40c-5cee-4d04-85c5-5a341c72460a',
-      'mode': 'no-cors',
-      }
+      Headers: {
+        'TRN-Api-Key': '213aa40c-5cee-4d04-85c5-5a341c72460a',
+      },
+      mode: 'no-cors',
     }
 
+    console.log('COMPONENT DID MOUNT');
     axios.get('https://public-api.tracker.gg/v2/apex/standard/profile/psn/whittlesey', options)
     .then(res => {
-      console.log(res);
+      console.log('response', res);
+      // const characters = res.data;
+      // this.setState({characters})
     })
+    .catch(err => console.error(err));
   }
 
   render(){
     return (
       <div className="player">
-          <p>This is a player</p>
+          <h1>Characters</h1>
+          <ul>
+            {/* { this.state.characters.map(char => <li>{char.name}</li>) } */}
+          </ul>
       </div>
     );
   }
